124cb6f2f170cf2a70c4daa83996af62064386b4
afifialaa <alaaahmedafifi@gmail.com>
2020-08-25 18:28:38 +0200

    selection sort typescript

---
+++ dsa/selectionSort.ts
@@ -0,0 +1,19 @@
+function selectionSort(list){
+    for(let i=0; i<list.length - 1; i++){
+        let min = i;
+        for(let u=0; u<list.length; u++){
+            if(list[u] > list[min]){
+                min = u;
+            }
+        }
+
+        let temp = list[i];
+        list[i] = list[min];
+        list[min] = temp;
+    }
+    return list;
+}
+
+let list = [4, 3, 5, 2, 1];
+let ans = selectionSort(list);
+console.log(ans);
